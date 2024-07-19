815644159f345873e32622ea226474d5e8ac0ceb
Liuda13 <luda.valko13@gmail.com>
2024-07-18 23:01:22 -0400

    add addCommand.java

--- src/day_1/addCommand.java
+++ src/day_1/addCommand.java
@@ -2,6 +2,8 @@ package day_1;
 
 public class addCommand {
     public static void main(String[] args) {
-        //I want to try git add filename
+
+
+        //I want to try git add filename.
     }
 }
