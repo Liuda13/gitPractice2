8fd7c3bf37114e1600d9ce27640fce634b144c43
Liuda13 <149981692+Liuda13@users.noreply.github.com>
2024-07-19 22:20:17 -0400

    Update HappyBirthday.java

--- src/HappyBirthday.java
+++ src/HappyBirthday.java
@@ -5,6 +5,8 @@ public class HappyBirthday {
         ty ty ty
 
             lalala
+              public static void main(String[] args) {
+        System.out.println("Happy birthday B18");
 
     }
 }
